5f0b8c7027e644507c2c0fb2cc5f93043db98cba
Katlin <kvsampso@purdue.edu>
2022-03-11 04:09:12 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='net_map',
+    version='2.1',
+    author='Katlin Sampson',
+    author_email='kvsampso@purdue.edu',
+    description='Creates network map and switch list',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.itap.purdue.edu/ITIS-Networking/net_map',
+    project_urls={
+        'Bug Tracker': 'https://github.itap.purdue.edu/ITIS-Networking/net_map/issues',
+    },
+    license="LICENSE.md",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=['nil_lib'],
+)
